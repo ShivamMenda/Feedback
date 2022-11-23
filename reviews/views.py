323aa95611c81073fb32f8e09bea2c21a415f7e9
Shivam Menda <shivammenda2002@gmail.com>
2022-11-23 22:17:12 +0530

    Basic POST and GET request setup done.

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def review(request):
+    if(request.method=="POST"):
+        username=request.POST['username']
+        print(username)
+        return render(request,"reviews/thank_you.html")
+    else:
+        return render(request,"reviews/review.html")
