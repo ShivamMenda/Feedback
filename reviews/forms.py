06496a4052c6070818340f7703b7be84c0fc4400
Shivam Menda <shivammenda2002@gmail.com>
2022-11-24 23:34:38 +0530

    Forms basic stup done.

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,3 +1,15 @@
 from django import forms
-class ReviewForm(forms.Form):
-    user_name=forms.CharField()
+from .models import Review
+# class ReviewForm(forms.Form):
+#     user_name=forms.CharField(label="Name")
+#     review_text=forms.CharField(label="review",widget=forms.Textarea,max_length=200)
+#     rating=forms.IntegerField(label="Your rating",min_value=1,max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model=Review
+        fields='__all__'
+        labels={
+            "user_name":"Your Name"
+        }
+        
--- reviews/views.py
+++ reviews/views.py
@@ -1,16 +1,19 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
+from .models import Review
 
 def review(request):
     if(request.method=="POST"):
         form=ReviewForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            form.save()
             return HttpResponseRedirect("/thank-you")
-    form=ReviewForm()
+    else:
+        form=ReviewForm()
+    
     return render(request,"reviews/review.html",{
-        "form":form
+    "form":form
         })
 
 def thank_you(request):
