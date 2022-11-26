6f3d8f76a8fb318680c9b53f433a7936a0d62df0
Shivam Menda <shivammenda2002@gmail.com>
2022-11-27 00:05:02 +0530

    Sessions done.

--- reviews/views.py
+++ reviews/views.py
@@ -29,4 +29,20 @@ class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
 
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        loaded_review=self.object  # type: ignore
+        request=self.request
+        fav_id=request.session.get("favorite_review")
+        context["is_fav"]= (fav_id==str(loaded_review.id))
+        return context
+    
+class AddFavView(View):
+    def post(self,request):
+        review_id=request.POST["review_id"]
+        request.session["favorite_review"]=review_id
+        return HttpResponseRedirect("/reviews/"+review_id)
+
+
+
 
